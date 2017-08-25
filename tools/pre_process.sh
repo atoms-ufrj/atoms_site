eee4a2fd8fcac76a80f609ba75231bd2993b38d8
Charlles Abreu <abreu@eq.ufrj.br>
2017-08-25 14:11:09 -0300

    Try download an image up to 20 times

--- tools/pre_process.sh
+++ tools/pre_process.sh
@@ -9,7 +9,12 @@ if [ $# == "0" ] || [ $1 == "people" ]; then
   address="http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id="
   for code in $(grep 'K[0-9]\{7\}[A-Z][0-9]' pages/people.md); do
     image="images/$code.jpg"
-    eval "wget -O $image \"$address$code\""
+    options="--retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --continue"
+    for i in $(seq 1 20); do
+      eval "wget $options -O $image \"$address$code\""
+      if [ $? = 0 ]; then break; fi; # check return value, break if successful (0)
+      sleep 1s;
+    done;
     width=$(identify -format "%w" $image)
     if [ "$width" -gt "200" ]; then
       percent=$(echo "20000/$width" | bc)
